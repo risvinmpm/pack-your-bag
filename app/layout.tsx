a85004ecd0488a38e8ba0fc0e2b0584b2be9d35d
risvinmpm <risvinmp39@gmail.com>
2025-04-02 16:30:30 +0530

    latest changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,28 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import type { Metadata } from 'next';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import './globals.css'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: "Pack Your Bag",
-  description: "travel",
-};
+  title: 'Travel',
+  description: 'Travel UI/UX App for Camping',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        <main className="relative overflow-hidden">{children}</main>
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
-  );
+  )
 }
