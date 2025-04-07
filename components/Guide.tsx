8ea6515bd6b3d30e658d935fd08c23580482ec26
risvinmpm <risvinmp39@gmail.com>
2025-04-07 11:14:53 +0530

    latest change

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -17,7 +17,7 @@ const Guide = () => {
       <div className="flexCenter max-container relative w-full">
         <Image src="/boat.png" alt='boat' width={1440} height={580} className='w-full object-cover object-center 2xl:rounded-5xl' />
         <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
-          <Image src="/meter.svg" alt='meter' width={16} height={158} />
+          <Image src="/meter.svg" alt='meter' width={16} height={158} className='h-full w-auto' />
           <div className="flexBetween flex-col">
             <div className="flex w-full flex-col">
               <div className="flexBetween w-full">
@@ -28,7 +28,7 @@ const Guide = () => {
             </div>
             <div className="flex w-full flex-col">
               <p className='regular-16 text-gray-20'>Start track</p>
-              <p className='bold-20 mt-2 whitespace-nowrap'>Wonorejo Pasuruan</p>
+              <h4 className='bold-20 mt-2 whitespace-nowrap'>Wonorejo Pasuruan</h4>
             </div>
           </div>
         </div>
